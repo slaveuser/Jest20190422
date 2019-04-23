aad8b41848b3f93d22b9fdd38232a728b99490c1
gmcpo <gmcpo@LAPTOP-2AD376QK>
2019-04-23 15:20:41 +0100

    Initial commit

---
+++ jest-common/src/main/java/io/searchbox/core/Update.java
@@ -0,0 +1,57 @@
+package io.searchbox.core;
+
+import io.searchbox.action.BulkableAction;
+import io.searchbox.action.SingleResultAbstractDocumentTargetedAction;
+import io.searchbox.client.config.ElasticsearchVersion;
+import io.searchbox.params.Parameters;
+
+/**
+ * @author Dogukan Sonmez
+ * @author cihat keser
+ */
+public class Update extends SingleResultAbstractDocumentTargetedAction implements BulkableAction<DocumentResult> {
+
+    protected Update(Builder builder) {
+        super(builder);
+        this.payload = builder.payload;
+    }
+
+    @Override
+    public String getBulkMethodName() {
+        return "update";
+    }
+
+    @Override
+    protected String buildURI(ElasticsearchVersion elasticsearchVersion) {
+        return super.buildURI(elasticsearchVersion) + "/_update";
+    }
+
+    @Override
+    public String getRestMethodName() {
+        return "POST";
+    }
+
+    @Override
+    public String getPathToResult() {
+        return "ok";
+    }
+
+    public static class Builder extends SingleResultAbstractDocumentTargetedAction.Builder<Update, Builder> {
+        private final Object payload;
+
+        public Builder(Object payload) {
+            this.payload = payload;
+        }
+
+        public Update build() {
+            return new Update(this);
+        }
+    }
+
+    public static class VersionBuilder extends Builder {
+        public VersionBuilder(Object payload, Long version) {
+            super(payload);
+            this.setParameter(Parameters.VERSION, version);
+        }
+    }
+}
